0fbacc140cf2245aad964f27e3bf59759daf5dff
work221q-beep <work221q@gmail.com>
2026-03-04 22:04:18 +0530

    Update db.py

--- db.py
+++ db.py
@@ -9,14 +9,14 @@ db = client.sylas_chaos
 
 server_configs = db.server_configs
 vuln_state = db.vulnerability_state 
+role_baselines = db.role_baselines # NEW: Stores permission baselines
 
 async def init_indexes():
-    # Ensures we only have one entry per vulnerability type per server
     await vuln_state.create_index([("server_id", 1), ("vuln_name", 1)], unique=True)
     await server_configs.create_index("server_id", unique=True)
+    await role_baselines.create_index([("server_id", 1), ("role_id", 1)], unique=True)
 
 async def upsert_vulnerability(server_id: str, vuln_name: str, is_vulnerable: bool, details: str):
-    """Updates the CURRENT state of a vulnerability. Overwrites old data."""
     status = "VULNERABLE" if is_vulnerable else "SECURE"
     await vuln_state.update_one(
         {"server_id": server_id, "vuln_name": vuln_name},
