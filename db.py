826999894e4f88cab54e40658d08f9d9b2536f36
work221q-beep <work221q@gmail.com>
2026-03-05 14:41:55 +0530

    Update db.py

--- db.py
+++ db.py
@@ -9,12 +9,16 @@ db = client.sylas_chaos
 
 server_configs = db.server_configs
 vuln_state = db.vulnerability_state 
-payload_armory = db.payload_armory # NEW: The Harvester Storage
+payload_armory = db.payload_armory
+guild_premium = db.guild_premium       # NEW: Tracks active subscriptions
+guild_cooldowns = db.guild_cooldowns   # NEW: Tracks wargame usage
 
 async def init_indexes():
     await vuln_state.create_index([("server_id", 1), ("vuln_name", 1)], unique=True)
     await server_configs.create_index("server_id", unique=True)
     await payload_armory.create_index("raid_type")
+    await guild_premium.create_index("guild_id", unique=True)
+    await guild_cooldowns.create_index([("guild_id", 1), ("raid_type", 1)], unique=True)
 
 async def upsert_vulnerability(server_id: str, vuln_name: str, is_vulnerable: bool, details: str):
     status = "VULNERABLE" if is_vulnerable else "SECURE"
